842b79a41c27fdbaae8a5ca96ea809293468805a
John Thompson <john@springframework.guru>
2024-04-02 17:01:59 -0400

    adding tests

--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -2,7 +2,9 @@ package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -11,6 +13,41 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @WebMvcTest
 public class BeerControllerIT extends BaseIT {
+
+    @Test
+    void initCreationFormNotAuthorized() throws Exception {
+        mockMvc.perform(get("/beers/new"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithUserDetails("spring")
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/beers/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+
+    }
+
+    @Test
+    void initCreationFormWithBasic() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("spring", "guru")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+
+    }
+
+    @Test
+    void initCreationFormWithBasicUser() throws Exception {
+        mockMvc.perform(get("/beers/new").with(httpBasic("user", "password")))
+                .andExpect(status().isOk())
+                .andExpect(view().name("beers/createBeer"))
+                .andExpect(model().attributeExists("beer"));
+    }
+
+
 
     @Test
     void findBeers() throws Exception{
