9058cbbee1b59c4d02ba478960c7fb88604af22c
wlidBirmou <abderrahim@klfgroup.ca>
2024-06-22 13:52:35 -0400

    Creation of SecurityConfig plus configuration of formlogin and basic auth for all the pages

---
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package guru.sfg.brewery.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequest -> {
+                authorizeRequest.antMatchers("/","/login","/webjars/**","/resources/**").permitAll();
+                })
+                .authorizeRequests().anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();    }
+}
