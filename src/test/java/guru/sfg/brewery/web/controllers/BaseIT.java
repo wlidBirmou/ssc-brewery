b3ccd59a0bc165b4711275f2c20917d4659dac22
wlidBirmou <abderrahim@klfgroup.ca>
2024-06-22 13:52:03 -0400

    Creation of test for basic auth for index page

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -0,0 +1,49 @@
+package guru.sfg.brewery.web.controllers;
+
+import guru.sfg.brewery.config.SecurityConfig;
+import guru.sfg.brewery.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerService;
+import guru.sfg.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+@Import(SecurityConfig.class)
+public abstract class BaseIT {
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+     MockMvc mockMvc;
+     @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+
+
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc= MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+    }
+
+
+}
