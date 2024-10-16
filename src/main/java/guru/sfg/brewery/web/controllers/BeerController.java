90bff6f3aa5f36162981412546899eddd5dcb32f
wlidBirmou <laakab@gmail.com>
2024-10-16 12:18:06 -0400

    Tests implementation

--- src/main/java/guru/sfg/brewery/web/controllers/BeerController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/BeerController.java
@@ -28,10 +28,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -52,6 +49,11 @@ public class BeerController {
     public String findBeers(Model model) {
         model.addAttribute("beer", Beer.builder().build());
         return "beers/findBeers";
+    }
+
+    @DeleteMapping("/{uuid}")
+    public void deleteBeerBy(UUID uuid){
+        this.beerRepository.deleteById(uuid);
     }
 
     @GetMapping
--- src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerControllerIT.java
@@ -5,12 +5,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest
 public class BeerControllerIT extends BaseIT{
+
+
+    @Test
+    void deleteSecurityTest() throws Exception{
+        this.mockMvc.perform(delete("/beers/97df0c39-90c4-4ae0-b663-453e8e19c311")
+                .header("api-key", "spring").header("api-secret","guru")).andExpect(status().isOk());
+    }
 
 
     @Test
